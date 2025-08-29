8f02cb314bc99575cdd9f6d611b8959da5276314
Edgardo Farias Franco <effranco@citivirt.citi.com.mx>
2025-08-28 22:47:50 -0600

    Agregué algunos get de Express para practicar sus funciones y entender la estructura

--- express_curso/index.js
+++ express_curso/index.js
@@ -19,13 +19,28 @@ app.get('/sume/:x/:y', (req, res) => {
 })
 
 app.get('/users/:username/photo', (req, res) => {
-
     console.log(req.params)
     if(req.params.username === 'edgardo') return res.send('Bienvenido Edgardo')
 
     res.send('El usuario no es valido...')
-    
+})
 
+app.get('/query/:username', (req, res) => {
+    console.log(req.query.user)
+    console.log(req.query.age)
+
+    res.send(`Hola ${req.params.username}`)
+})
+
+app.use((req, res, next) => {
+    console.log('Paso por aqui')
+
+    next() // es como un break para la funcion, esto viene de express
+})
+
+app.get('/search', (req, res) => {
+    if (req.query.q === 'Hola mundo') return res.send('Lista de libros de JS')
+    else res.send('Pagina normal')
 })
 
 app.listen(3000)
